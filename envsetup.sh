93c24a7143950e5fc08b70f677942b455ab7355e
pageask <pageask@gmail.com>
2012-07-17 17:32:47 +0800

    add envsetup.sh

---
+++ envsetup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -
+# envsetup.sh
+#
+# author : hzhiwen
+# date   : 20120625
+#
+
+export TOOLS_HOME=/home/pageask/tools
+export PATH=$TOOLS_HOME:$PATH
+
+export JAVA_HOME=$TOOLS_HOME/java/jdk1.6.0_31
+export PATH=$JAVA_HOME/bin:$PATH
+
+export SCRIPTS_HOME=$TOOLS_HOME/scripts
+export PATH=$SCRIPTS_HOME:$PATH
+
+export ECLIPSE_HOME=$TOOLS_HOME/eclipse
+export PATH=$ECLIPSE_HOME:$PATH
+
+export M2_HOME=$TOOLS_HOME/maven/apache-maven-3.0.4
+export M2=$M2_HOME/bin
+#export MAVEN_OPTS="-Xms256m -Xmx512m"
+export PATH=$M2:$PATH
