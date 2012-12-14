498717c1a18097a2b5605e9d761e475e6b094c8d
pageask <pageask@gmail.com>
2012-12-14 17:18:26 +0800

    nightly-build.sh add some target

--- generate-numbers.pl
+++ generate-numbers.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 $initial = 19;
-$step = 22;
+$step = 25;
 
 for($i = 0; $i < 27; $i++) {
     print "<item>$initial</item>\n";
--- nightly-build.sh
+++ nightly-build.sh
@@ -1,27 +1,39 @@
 #!/bin/bash -
 
-cd ~/projects/release/gt-i9100-15a-d
+#cd ~/projects/release/gt-i9100-15a-d
 #sm-alps.sh mtk75_ics gt-i9100-15a-d eng
-sm-alps.sh mtk75_ics gt-i9100-15a-d user
+#sm-alps.sh mtk75_ics gt-i9100-15a-d user
 
-cd ~/projects/release/gt-i9100-15a-s
+#cd ~/projects/release/gt-i9100-15a-s
 #sm-alps.sh mtk75_ics gt-i9100-15a-s eng
-sm-alps.sh mtk75_ics gt-i9100-15a-s user
+#sm-alps.sh mtk75_ics gt-i9100-15a-s user
 
-cd ~/projects/release/gt-i9100-75-d
+#cd ~/projects/release/gt-i9100-75-d
 #sm-alps.sh mtk75_ics gt-i9100-75-d eng
-sm-alps.sh mtk75_ics gt-i9100-75-d user
+#sm-alps.sh mtk75_ics gt-i9100-75-d user
 
-cd ~/projects/release/gt-i9100-75-s
+#cd ~/projects/release/gt-i9100-75-s
 #sm-alps.sh mtk75_ics gt-i9100-75-s eng
-sm-alps.sh mtk75_ics gt-i9100-75-s user
+#sm-alps.sh mtk75_ics gt-i9100-75-s user
 
-cd ~/projects/release/gt-i9300-1023-75-d
+#cd ~/projects/release/gt-i9300-1023-75-d
 #sm-alps.sh mht75_ics gt-i9300-1023-75-d eng
-sm-alps.sh mht75_ics gt-i9300-1023-75-d user
+#sm-alps.sh mht75_ics gt-i9300-1023-75-d user
 
-cd ~/projects/release/gt-i9300-1023-75-s
+#cd ~/projects/release/gt-i9300-1023-75-s
 #sm-alps.sh mht75_ics gt-i9300-1023-75-s eng
-sm-alps.sh mht75_ics gt-i9300-1023-75-s user
+#sm-alps.sh mht75_ics gt-i9300-1023-75-s user
+
+cd ~/projects/release/gt-i9300-1023-75-s-cheng
+#sm-alps.sh mht75_ics gt-i9300-1023-75-s-cheng eng
+sm-alps.sh mht75_ics gt-i9300-1023-75-s-cheng user
+
+cd ~/projects/release/gt-i9300-1023-75-s-cheng-en
+#sm-alps.sh mht75_ics gt-i9300-1023-75-s-cheng-en eng
+sm-alps.sh mht75_ics gt-i9300-1023-75-s-cheng-en user
+
+cd ~/projects/release/gt-i9300-1023-75-s-wu
+#sm-alps.sh mht75_ics gt-i9300-1023-75-s-wu eng
+sm-alps.sh mht75_ics gt-i9300-1023-75-s-wu user
 
 #shutdown -h now
