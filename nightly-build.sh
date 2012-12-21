bb6695a669558ba37f1ab8f722b04efd08c46707
pageask <pageask@gmail.com>
2012-12-21 20:43:20 +0800

    build a02-77

--- nightly-build.sh
+++ nightly-build.sh
@@ -24,16 +24,20 @@
 #sm-alps.sh mht75_ics gt-i9300-1023-75-s eng
 #sm-alps.sh mht75_ics gt-i9300-1023-75-s user
 
-cd ~/projects/release/gt-i9300-1023-75-s-cheng
+#cd ~/projects/release/gt-i9300-1023-75-s-cheng
 #sm-alps.sh mht75_ics gt-i9300-1023-75-s-cheng eng
-sm-alps.sh mht75_ics gt-i9300-1023-75-s-cheng user
+#sm-alps.sh mht75_ics gt-i9300-1023-75-s-cheng user
 
-cd ~/projects/release/gt-i9300-1023-75-s-cheng-en
+#cd ~/projects/release/gt-i9300-1023-75-s-cheng-en
 #sm-alps.sh mht75_ics gt-i9300-1023-75-s-cheng-en eng
-sm-alps.sh mht75_ics gt-i9300-1023-75-s-cheng-en user
+#sm-alps.sh mht75_ics gt-i9300-1023-75-s-cheng-en user
 
-cd ~/projects/release/gt-i9300-1023-75-s-wu
+#cd ~/projects/release/gt-i9300-1023-75-s-wu
 #sm-alps.sh mht75_ics gt-i9300-1023-75-s-wu eng
-sm-alps.sh mht75_ics gt-i9300-1023-75-s-wu user
+#sm-alps.sh mht75_ics gt-i9300-1023-75-s-wu user
+
+cd ~/projects/release/a02-77
+#sm-alps.sh mobitek77_m01_ics2 a02-77 eng
+sm-alps.sh mobitek77_m01_ics2 a02-77 user
 
 #shutdown -h now
